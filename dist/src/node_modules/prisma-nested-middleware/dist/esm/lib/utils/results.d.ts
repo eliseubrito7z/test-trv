8e9b30aee463609db6133b7c74c4c9a088aacb98
Eliseu Brito <eliseubrito776@gmail.com>
2024-04-06 00:25:16 -0300

    Delete dist/src/node_modules directory

--- dist/src/node_modules/prisma-nested-middleware/dist/esm/lib/utils/results.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function addIdSymbolsToResult(result: any, parentId?: number, startId?: number): number;
-export declare function stripIdSymbolsFromResult(result: any): void;
-export declare function getRelationResult(result: any, relations: string[]): any;
-export declare function updateResultRelation(result: any, relation: string, relationResult: any): any;
